aee93959fc93dbf07a556f0e57b322924d551aeb
hardzcymz91 <76249462+hardzcymz91@users.noreply.github.com>
2021-01-04 19:09:44 +0700

    Delete botConfig.js

--- botConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    token: "Nzk1MTQ3MDEzODM0MTQ1ODEy.X_FIXQ.znsz8kt3uzCAqjqvgPuUf-L2328",
-    prefix: "a?",
-    admins: [
-        "795605552545136652"
-],
-    debug: true,
-    countChannel: "countChannelID"
-};
